4aaf994ec2f1c26945ddddc61ecc2ae541352e1f
Alejandro Jara <alejaraberra@hotmail.com>
2023-08-31 09:59:10 -0400

    .#

--- SGHUA/Form1.Designer.cs
+++ SGHUA/Form1.Designer.cs
@@ -46,72 +46,77 @@
             // Idpersona
             // 
             Idpersona.AutoSize = true;
-            Idpersona.Location = new Point(50, 69);
+            Idpersona.Location = new Point(57, 92);
             Idpersona.Name = "Idpersona";
-            Idpersona.Size = new Size(59, 15);
+            Idpersona.Size = new Size(75, 20);
             Idpersona.TabIndex = 0;
             Idpersona.Text = "Idpersona";
             // 
             // Nombre
             // 
             Nombre.AutoSize = true;
-            Nombre.Location = new Point(240, 69);
+            Nombre.Location = new Point(274, 92);
             Nombre.Name = "Nombre";
-            Nombre.Size = new Size(51, 15);
+            Nombre.Size = new Size(64, 20);
             Nombre.TabIndex = 1;
             Nombre.Text = "Nombre";
             // 
             // Apellido
             // 
             Apellido.AutoSize = true;
-            Apellido.Location = new Point(405, 69);
+            Apellido.Location = new Point(463, 92);
             Apellido.Name = "Apellido";
-            Apellido.Size = new Size(51, 15);
+            Apellido.Size = new Size(66, 20);
             Apellido.TabIndex = 2;
             Apellido.Text = "Apellido";
             // 
             // telefono
             // 
             telefono.AutoSize = true;
-            telefono.Location = new Point(614, 69);
+            telefono.Location = new Point(702, 92);
             telefono.Name = "telefono";
-            telefono.Size = new Size(51, 15);
+            telefono.Size = new Size(65, 20);
             telefono.TabIndex = 3;
             telefono.Text = "telefono";
             // 
             // txtidpersona
             // 
-            txtidpersona.Location = new Point(31, 87);
+            txtidpersona.Location = new Point(35, 116);
+            txtidpersona.Margin = new Padding(3, 4, 3, 4);
             txtidpersona.Name = "txtidpersona";
-            txtidpersona.Size = new Size(100, 23);
+            txtidpersona.Size = new Size(114, 27);
             txtidpersona.TabIndex = 4;
             // 
             // txtnombre
             // 
-            txtnombre.Location = new Point(216, 87);
+            txtnombre.Location = new Point(247, 116);
+            txtnombre.Margin = new Padding(3, 4, 3, 4);
             txtnombre.Name = "txtnombre";
-            txtnombre.Size = new Size(100, 23);
+            txtnombre.Size = new Size(114, 27);
             txtnombre.TabIndex = 5;
             // 
             // txtapellido
             // 
-            txtapellido.Location = new Point(382, 87);
+            txtapellido.Location = new Point(437, 116);
+            txtapellido.Margin = new Padding(3, 4, 3, 4);
             txtapellido.Name = "txtapellido";
-            txtapellido.Size = new Size(100, 23);
+            txtapellido.Size = new Size(114, 27);
             txtapellido.TabIndex = 6;
             // 
             // txttelefono
             // 
-            txttelefono.Location = new Point(586, 87);
+            txttelefono.Location = new Point(670, 116);
+            txttelefono.Margin = new Padding(3, 4, 3, 4);
             txttelefono.Name = "txttelefono";
-            txttelefono.Size = new Size(100, 23);
+            txttelefono.Size = new Size(114, 27);
             txttelefono.TabIndex = 7;
             // 
             // btnguardar
             // 
-            btnguardar.Location = new Point(128, 146);
+            btnguardar.Location = new Point(146, 195);
+            btnguardar.Margin = new Padding(3, 4, 3, 4);
             btnguardar.Name = "btnguardar";
-            btnguardar.Size = new Size(75, 23);
+            btnguardar.Size = new Size(86, 31);
             btnguardar.TabIndex = 8;
             btnguardar.Text = "guardar";
             btnguardar.UseVisualStyleBackColor = true;
@@ -119,18 +124,20 @@
             // 
             // btneditar
             // 
-            btneditar.Location = new Point(317, 146);
+            btneditar.Location = new Point(362, 195);
+            btneditar.Margin = new Padding(3, 4, 3, 4);
             btneditar.Name = "btneditar";
-            btneditar.Size = new Size(75, 23);
+            btneditar.Size = new Size(86, 31);
             btneditar.TabIndex = 9;
             btneditar.Text = "editar";
             btneditar.UseVisualStyleBackColor = true;
             // 
             // btnelimiar
             // 
-            btnelimiar.Location = new Point(502, 146);
+            btnelimiar.Location = new Point(574, 195);
+            btnelimiar.Margin = new Padding(3, 4, 3, 4);
             btnelimiar.Name = "btnelimiar";
-            btnelimiar.Size = new Size(75, 23);
+            btnelimiar.Size = new Size(86, 31);
             btnelimiar.TabIndex = 10;
             btnelimiar.Text = "eliminar";
             btnelimiar.UseVisualStyleBackColor = true;
@@ -138,18 +145,20 @@
             // dgvpersonas
             // 
             dgvpersonas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvpersonas.Location = new Point(117, 192);
+            dgvpersonas.Location = new Point(134, 256);
+            dgvpersonas.Margin = new Padding(3, 4, 3, 4);
             dgvpersonas.Name = "dgvpersonas";
+            dgvpersonas.RowHeadersWidth = 51;
             dgvpersonas.RowTemplate.Height = 25;
-            dgvpersonas.Size = new Size(523, 246);
+            dgvpersonas.Size = new Size(598, 328);
             dgvpersonas.TabIndex = 11;
             dgvpersonas.CellContentClick += dgvpersonas_CellContentClick;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(870, 563);
             Controls.Add(dgvpersonas);
             Controls.Add(btnelimiar);
             Controls.Add(btneditar);
@@ -162,6 +171,7 @@
             Controls.Add(Apellido);
             Controls.Add(Nombre);
             Controls.Add(Idpersona);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dgvpersonas).EndInit();

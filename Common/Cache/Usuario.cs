04e782b0b79162218f934eda90e0558bcdfac9d0
Thiago Araujo <79669455+RadScorpion17@users.noreply.github.com>
2023-09-19 17:37:26 -0400

    Update Usuario.cs

--- Common/Cache/Usuario.cs
+++ Common/Cache/Usuario.cs
@@ -14,7 +14,6 @@ namespace Common.Cache
         public string? Nombre { get; set; }
         public string? Apellido { get; set; }
         public int? Rol { get; set; }
-        public string? Email { get; set; }
         public int? Genero { get; set; }
         public DateTime? Nacimiento { get; set; }
         public string? Cedula { get; set; }

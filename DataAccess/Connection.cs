08d0acbc030b01440d82221874d7d3c807578550
Thiago Araujo <79669455+RadScorpion17@users.noreply.github.com>
2023-09-19 16:45:51 -0400

    Update Connection.cs

--- DataAccess/Connection.cs
+++ DataAccess/Connection.cs
@@ -8,11 +8,7 @@ namespace DataAccess
 
         public Connection()
         {
-<<<<<<< HEAD
-            var = "Host=localhost;Username=postgres;Password=258017;Database=postgres";
-=======
             var = "Host=localhost;Username=postgres;Password=;Database=postgres";
->>>>>>> c5941904ddbba48a6c1514a4816665ed15d86510
         }
 
         protected NpgsqlConnection GetConnection()
